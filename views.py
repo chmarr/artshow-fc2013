cbd2d5085ec484bf426de18aba5416bfa498c87b
Chris Cogdon <chris@cogdon.org>
2013-11-24 20:48:34 -0800

    Adding option to show waiting-list artists with a "*".

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from artshow.models import Artist
 from django.shortcuts import render
+from django.conf import settings
 
 
 def artists(request):
@@ -8,4 +9,5 @@ def artists(request):
     aa = [a for a in aa if a.is_active()]
     aa.sort(key=lambda x: x.artistname().lower())
     column_length = (len(aa) + 2) / 3
-    return render(request, "furcon/artists.html", {"artists": aa, "column_length": column_length})
+    return render(request, "furcon/artists.html", {"artists": aa, "column_length": column_length,
+                                                   "mark_waiting": settings.FURCON_ARTIST_LISTING_MARK_WAITING})

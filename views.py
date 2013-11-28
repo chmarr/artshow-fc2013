f48d034a60e09895a388c0bf79c4aedfe9c8de14
Chris Cogdon <chris@cogdon.org>
2013-11-28 15:14:28 -0800

    Switched mark_waiting option from settings to a query parameter on the view.

--- views.py
+++ views.py
@@ -1,13 +1,13 @@
 from artshow.models import Artist
 from django.shortcuts import render
-from django.conf import settings
 
 
 def artists(request):
+    mark_waiting = bool(request.GET.get("mark_waiting", ""))
     aa = Artist.objects.all()
     aa = list(aa)
     aa = [a for a in aa if a.is_active()]
     aa.sort(key=lambda x: x.artistname().lower())
     column_length = (len(aa) + 2) / 3
     return render(request, "furcon/artists.html", {"artists": aa, "column_length": column_length,
-                                                   "mark_waiting": settings.FURCON_ARTIST_LISTING_MARK_WAITING})
+                                                   "mark_waiting": mark_waiting})
